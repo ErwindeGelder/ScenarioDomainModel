5fa634297dc2a90aab1bbaf7162052581b8a1850
Erwin de Gelder <erwin.degelder@tno.nl>
2018-10-13 15:42:46 +0800

    Changed whole structure to reduce memory usage

--- 20180319 Completeness/mise_bivariate.py
+++ 20180319 Completeness/mise_bivariate.py
@@ -1,7 +1,7 @@
 import numpy as np
 from gaussianmixture import GaussianMixture
 import os
-# from matplotlib2tikz import save
+from matplotlib2tikz import save
 import h5py
 from tqdm import tqdm
 from fastkde import KDE
@@ -10,16 +10,17 @@ import matplotlib.pyplot as plt
 # Parameters
 nmax = 5000
 nmin = 100
-nstep = 100
-npdf = 61
+nsteps = 50
+npdf = 101
 seed = 0
-nrepeat = 100
+nrepeat = 200
 overwrite = False
 xlim = [-3, 3]
 mua = [-1, 1]
 mub = [-0.5, 0.5, 1.5]
 sigmaa = [0.5, 0.3]
 sigmab = [0.3, 0.5, 0.3]
+max_change_bw = 0.25
 
 # Create object for generating data from a Gaussian mixture
 gma = GaussianMixture(mua, sigmaa)
@@ -30,7 +31,7 @@ np.random.seed(seed)
 A = gma.generate_samples(nmax*nrepeat).reshape((nrepeat, nmax))
 B = gmb.generate_samples(nmax*nrepeat).reshape((nrepeat, nmax))
 AB = np.concatenate((A[:, :, np.newaxis], B[:, :, np.newaxis]), axis=2)
-nn = np.arange(nmin, nmax+nstep, nstep)
+nn = np.round(np.logspace(np.log10(nmin), np.log10(nmax), nsteps)).astype(np.int)
 
 # Create the real pdfs
 (xpdf,), ypdfa = gma.pdf(minx=[xlim[0]], maxx=[xlim[1]], n=npdf)
@@ -48,33 +49,31 @@ if overwrite or not os.path.exists(filename):
     # This has quite some influence on the remainder of the procedure...
     i_bw = 0
 
-    # For now, just set the data for all the kdes
-    print("Initialize all {:d} KDEs".format(nrepeat))
-    kdesa = [KDE(data=x) for x in A]
-    kdesb = [KDE(data=x) for x in B]
-    kdesab = [KDE(data=x) for x in AB]
-    for kdea, kdeb, kdeab in tqdm(zip(kdesa, kdesb, kdesab)):
-        kdea.set_score_samples(xpdf)
-        kdeb.set_score_samples(xpdf)
-        kdeab.set_score_samples(xpdfab)
-
-    # Compute the bandwidths
+    # Compute the bandwidth
     print("Compute the bandwidths")
+    kdea = KDE(data=A[i_bw])
+    kdeb = KDE(data=B[i_bw])
+    kdeab = KDE(data=AB[i_bw])
     for i, n in enumerate(tqdm(nn)):
         # Compute the optimal bandwidth using 1-leave-out
-        kdesa[i_bw].set_n(n)
-        kdesa[i_bw].compute_bw()
-        bwa[i] = kdesa[i_bw].bw
-        kdesb[i_bw].set_n(n)
-        kdesb[i_bw].compute_bw()
-        bwb[i] = kdesb[i_bw].bw
-        kdesab[i_bw].set_n(n)
-        kdesab[i_bw].compute_bw()
-        bwab[i] = kdesab[i_bw].bw
+        kdea.set_n(n)
+        kdeb.set_n(n)
+        kdeab.set_n(n)
+        if i == 0:
+            kdea.compute_bw()
+            kdeb.compute_bw()
+            kdeab.compute_bw()
+        else:
+            kdea.compute_bw(min_bw=kdea.bw*(1-max_change_bw), max_bw=kdea.bw*(1+max_change_bw))
+            kdeb.compute_bw(min_bw=kdeb.bw*(1-max_change_bw), max_bw=kdeb.bw*(1+max_change_bw))
+            kdeab.compute_bw(min_bw=kdeab.bw*(1-max_change_bw), max_bw=kdeab.bw*(1+max_change_bw))
+        bwa[i] = kdea.bw
+        bwb[i] = kdeb.bw
+        bwab[i] = kdeab.bw
 
     # Get the constants mu_k
-    muka = kdesa[0].muk
-    mukab = kdesab[0].muk
+    muka = kdea.muk
+    mukab = kdeab.muk
 
     # Compute all the nrepeats pdfs. We need to do this many times in order to determine the real MISE.
     # At the same time (to save memory), the MISE is computed
@@ -84,20 +83,28 @@ if overwrite or not os.path.exists(filename):
     est_mise_ind = np.zeros_like(est_mise_dep)
     print("Estimate pdfs")
     for j in tqdm(range(nrepeat)):
+        # Initialize the KDE
+        kdea = KDE(data=A[j])
+        kdeb = KDE(data=B[j])
+        kdeab = KDE(data=AB[j])
+        kdea.set_score_samples(xpdf)
+        kdeb.set_score_samples(xpdf)
+        kdeab.set_score_samples(xpdfab)
+
         for i, n in enumerate(nn):
             # Compute the pdfs and the Laplacians
-            kdesa[j].set_n(n)
-            kdesa[j].set_bw(bwa[i])
-            pdfa_est = kdesa[j].score_samples()
-            laplaciana = kdesa[j].laplacian()
-            kdesb[j].set_n(n)
-            kdesb[j].set_bw(bwb[i])
-            pdfb_est = kdesb[j].score_samples()
-            laplacianb = kdesb[j].laplacian()
-            kdesab[j].set_n(n)
-            kdesab[j].set_bw(bwab[i])
-            pdfab_est = kdesab[j].score_samples()
-            laplacianab = kdesab[j].laplacian()
+            kdea.set_n(n)
+            kdea.set_bw(bwa[i])
+            pdfa_est = kdea.score_samples()
+            laplaciana = kdea.laplacian()
+            kdeb.set_n(n)
+            kdeb.set_bw(bwb[i])
+            pdfb_est = kdeb.score_samples()
+            laplacianb = kdeb.laplacian()
+            kdeab.set_n(n)
+            kdeab.set_bw(bwab[i])
+            pdfab_est = kdeab.score_samples()
+            laplacianab = kdeab.laplacian()
 
             # Compute the real MISE and estimate the MISE of the bivariate distribution
             real_mise_dep[i, j] = np.trapz(np.trapz((pdfab_est - ypdfab) ** 2, xpdf), xpdf)
@@ -149,27 +156,55 @@ print("Power for real MISE independent:      {:.2f}".format(np.polyfit(np.log(nn
 print("Power for estimated MISE independent: {:.2f}".format(np.polyfit(np.log(nn), np.log(est_mise_ind[:, 0]), 1)[0]))
 
 # Plot the MISEs
+sigmas = 3
 f, ax = plt.subplots(1, 1, figsize=(7, 5))
-plotreal = ax.loglog(nn, real_mise_dep, lw=5, label="Real MISE")
-plotest = ax.loglog(nn, est_mise_dep[:, 0], lw=5, label="Estimated MISE")
+plotreal = ax.loglog(nn, real_mise_dep, lw=5, label="Real MISE", color=[0, 0, 0])
+plotest = ax.loglog(nn, est_mise_dep[:, 0], lw=5, label="Estimated MISE", color=[.5, .5, .5])
+mean = np.mean(est_mise_dep, axis=1)
+std = np.std(est_mise_dep, axis=1)
+ax.fill_between(nn, mean - 3 * std, mean + 3 * std, color=[.8, .8, .8])
 ax.loglog(nn, real_mise_ind, '--', lw=5, color=plotreal[0].get_color())
 ax.loglog(nn, est_mise_ind[:, 0], '--', lw=5, color=plotest[0].get_color())
+mean = np.mean(est_mise_ind, axis=1)
+std = np.std(est_mise_ind, axis=1)
+ax.fill_between(nn, mean - 3 * std, mean + 3 * std, color=[.8, .8, .8])
 ax.set_xlabel("Number of samples")
 ax.set_ylabel("MISE")
-ax.legend()
 ax.grid(True)
 ax.set_xlim([np.min(nn), np.max(nn)])
-ax.set_title("Data to be assumed dependent (solid) or independent (dashed)")
-plt.show()
+# ax.set_title("Data to be assumed dependent (solid) or independent (dashed)")
 
 # Plot the bandwidth for varying n
+dx = np.mean(np.gradient(xpdf))
+lapa = np.trapz((np.gradient(np.gradient(ypdfa)) / dx**2)**2, xpdf)
+muka = 1 / (2*np.sqrt(np.pi))
+hopta = (1*muka / (nn*lapa))**(1/(1+4))
+lapb = np.trapz((np.gradient(np.gradient(ypdfb)) / dx**2)**2, xpdf)
+hoptb = (1*muka / (nn*lapb))**(1/(1+4))
+lapab = np.trapz(np.trapz((np.gradient(np.gradient(ypdfab, axis=0), axis=0) / dx**2 +
+                           np.gradient(np.gradient(ypdfab, axis=1), axis=1) / dx**2)**2, xpdf), xpdf)
+mukab = 1 / (4*np.pi)
+hoptab = ((2*mukab) / (nn*lapab))**(1/(2+4))
+
 _, ax = plt.subplots(1, 1, figsize=(7, 5))
-ax.semilogx(nn, bwa, label="bwa", lw=5)
-ax.semilogx(nn, bwb, label="bwb", lw=5)
-ax.semilogx(nn, bwab, label="bwab", lw=5)
+ax.loglog(nn, bwa, '-', label="bwa", lw=5, color=[.5, .5, .5])
+# ax.loglog(nn, hopta)
+ax.loglog(nn, bwb, ':', label="bwb", lw=5, color=[.5, .5, .5])
+# ax.loglog(nn, hoptb)
+ax.loglog(nn, bwab, '--', label="bwab", lw=5, color=[0, 0, 0])
+# ax.loglog(nn, hoptab)
 ax.set_xlabel("Number of samples")
 ax.set_ylabel("Bandwidth")
 ax.grid(True)
-ax.legend()
 ax.set_xlim([np.min(nn), np.max(nn)])
 plt.show()
+
+# Make a figure of the distribution and export to tikz
+_, ax = plt.subplots(1, 1, figsize=(7, 5))
+ax.plot(xpdf, ypdfa, lw=5, color=[0, 0, 0])
+ax.plot(xpdf, ypdfb, '--', lw=5, color=[.5, .5, .5])
+ax.grid(True)
+ax.set_xlabel('$x$')
+ax.set_xlim(xlim)
+save(os.path.join('..', '20180924 Completeness paper', 'figures', 'true_pdf.tikz'),
+     figureheight='\\figureheight', figurewidth='\\figurewidth')
