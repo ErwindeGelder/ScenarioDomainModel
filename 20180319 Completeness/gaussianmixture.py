00c66eb9b4416e2d08b1ddc262fb16c61bf506a3
Erwin de Gelder <erwin.degelder@tno.nl>
2018-11-07 14:05:29 +0100

    Add comments, make PEP8 compliant.

--- 20180319 Completeness/gaussianmixture.py
+++ 20180319 Completeness/gaussianmixture.py
@@ -1,111 +1,141 @@
+"""
+Guassian Mixture
+
+Author
+------
+Erwin de Gelder
+
+Creation
+--------
+08 Aug 2018
+
+
+To do
+-----
+
+
+Modifications
+-------------
+07 Nov 2018 Make PEP8 compliant
+
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 # Class for generating data with a mixture of Gaussians (GM)
 class GaussianMixture:
-    def __init__(self, mu, sigma, weights=None):
-        """ Initialise the class
+    """ Gaussian mixture
 
-        :param mu: containing the means of the individual Gaussians
-        :param sigma: containing the variances of the individual Gaussians
-        :param weights: optional weight for the Gaussians. By defaults, the gaussians are equally weighted
-        """
-
-        self.mu = np.array(mu)
+    Attributes:
+        mean(np.ndarray): Means of the various Gaussians.
+        sigma(np.ndarray): Covariances of the various Gaussians.
+        weights(np.ndarray): Weights of the various Gaussians.
+        cweights(np.ndarray): Cumulative weights of the various Gaussians.
+        dim(int): Dimension of the Gaussian Mixture.
+        k(int): Number of Gaussians.
+        chol(np.ndarray): Cholesky factorization that is needed for generation the Gaussians.
+    """
+    def __init__(self, mu: np.ndarray, sigma: np.ndarray, weights: np.ndarray = None):
+        self.mean = np.array(mu)
         self.sigma = np.array(sigma)
         if weights is None:
-            self.weights = np.ones(len(self.mu)) / len(self.mu)
+            self.weights = np.ones(len(self.mean)) / len(self.mean)
         else:
             self.weights = np.array(weights) / np.sum(weights)
         self.cweights = np.cumsum(self.weights)
 
         # Determine the dimension of the GM
-        if len(self.mu.shape) == 1:
-            self.d = 1
+        if len(self.mean.shape) == 1:
+            self.dim = 1
         else:
-            self.d = len(self.mu[0])
+            self.dim = len(self.mean[0])
 
         # Check if mu and sigma has right shape
-        if len(self.mu.shape) == 1:
-            self.mu = self.mu[:, np.newaxis]
+        if len(self.mean.shape) == 1:
+            self.mean = self.mean[:, np.newaxis]
         if len(self.sigma.shape) == 1:
             self.sigma = self.sigma[:, np.newaxis, np.newaxis]
 
         # Number of Gaussians
-        self.k = self.mu.shape[0]
+        self.k = self.mean.shape[0]
 
         # Perform Cholesky factorization that is needed for generation the Gaussians
         self.chol = np.linalg.cholesky(self.sigma)
 
-    def generate_samples(self, n):
+    def generate_samples(self, nsamples: int) -> np.ndarray:
         """ Generate samples from the mixture of Gaussians
 
-        :param n: Number of samples
+        :param nsamples: Number of samples
         :return y: n samples from the mixture of Gaussians
         """
 
         # Determine the index of the gaussian that is to be used
-        idx = np.searchsorted(self.cweights, np.random.rand(n))
+        idx = np.searchsorted(self.cweights, np.random.rand(nsamples))
 
         # Produce the samples
-        x = np.random.randn(n, self.d)
-        return self.mu[idx] + np.einsum('nij,nj->ni', self.chol[idx], x)
+        xsamples = np.random.randn(nsamples, self.dim)
+        return self.mean[idx] + np.einsum('nij,nj->ni', self.chol[idx], xsamples)
 
-    def pdf(self, n=51, minx=None, maxx=None):
+    def pdf(self, npoints: int = 51, minx: float = None, maxx: float = None):
         """ Generate probability density function (PDF)
 
-        :param n: Number of points to be used for each dimension (default=101)
-        :param minx: Lower limit of grid
-        :param maxx: Upper limit of grid
-        :return x: x values of the pdf
-        :return y: height of the pdf
+        :param npoints: Number of points to be used for each dimension (default=101).
+        :param minx: Lower limit of grid.
+        :param maxx: Upper limit of grid.
+        :return x: x values of the pdf.
+        :return y: height of the pdf.
         """
 
         # Set boundaries of grid
         if minx is None:
-            minx = np.zeros(self.d)
-            for i in range(self.d):
-                minx[i] = np.min(self.mu[:, i] - 3*np.sqrt(self.sigma[:, i, i]))
+            minx = np.zeros(self.dim)
+            for i in range(self.dim):
+                minx[i] = np.min(self.mean[:, i] - 3 * np.sqrt(self.sigma[:, i, i]))
         if maxx is None:
             maxx = np.zeros_like(minx)
-            for i in range(self.d):
-                maxx[i] = np.max(self.mu[:, i] + 3*np.sqrt(self.sigma[:, i, i]))
+            for i in range(self.dim):
+                maxx[i] = np.max(self.mean[:, i] + 3 * np.sqrt(self.sigma[:, i, i]))
 
         # Create the PDF grid
-        xorig = np.meshgrid(*[np.linspace(minx[i], maxx[i], n) for i in range(self.d)])
-        x = np.reshape(xorig, (self.d, n**self.d))
+        xorig = np.meshgrid(*[np.linspace(minx[i], maxx[i], npoints)
+                              for i in range(self.dim)])  # type: Tuple
+        marginal_grid = np.reshape(xorig, (self.dim, npoints ** self.dim))
 
         # Compute determinants and inverses of covariances (needed for computation of PDF)
         det = np.linalg.det(self.sigma)
         invsigma = np.linalg.inv(self.sigma)
 
         # Evaluate the PDF
-        xk = np.kron(x[np.newaxis, :, :], np.ones((self.k, 1, 1)))
-        mun = np.kron(self.mu[:, :, np.newaxis], np.ones((1, 1, n**self.d)))
-        exp = np.exp(-0.5 * np.einsum('kjn,kjn->kn', np.einsum('kin,kij->kjn', xk - mun, invsigma), xk - mun))
-        normalization = 1 / np.sqrt((2*np.pi)**self.d * det)
+        grid = np.kron(marginal_grid[np.newaxis, :, :], np.ones((self.k, 1, 1)))
+        mun = np.kron(self.mean[:, :, np.newaxis], np.ones((1, 1, npoints ** self.dim)))
+        exp = np.exp(-0.5 * np.einsum('kjn,kjn->kn',
+                                      np.einsum('kin,kij->kjn', grid - mun, invsigma),
+                                      grid - mun))
+        normalization = 1 / np.sqrt((2*np.pi) ** self.dim * det)
         pdf = np.einsum('kn,k->n', np.einsum('k,kn->kn', normalization, exp), self.weights)
-        return xorig, np.reshape(pdf, tuple([n for _ in range(self.d)]))
+        return xorig, np.reshape(pdf, tuple([npoints for _ in range(self.dim)]))
 
 
 if __name__ == "__main__":
     np.random.seed(0)
-    gm = GaussianMixture([-1, 1], [0.5, 0.5])
-    y = gm.generate_samples(10000)
-    plt.hist(y, 100, density=True)
-    (xpdf,), ypdf = gm.pdf()
-    plt.plot(xpdf, ypdf)
-    plt.xlim([xpdf[0], xpdf[-1]])
+    GM = GaussianMixture(np.array([-1, 1]), np.array([0.5, 0.5]))
+    YDATA = GM.generate_samples(10000)
+    plt.hist(YDATA, 100, density=True)
+    (XPDF,), YPDF = GM.pdf()
+    plt.plot(XPDF, YPDF)
+    plt.xlim([XPDF[0], XPDF[-1]])
     plt.title("Histogram of data with plot of real PDF")
     plt.show()
 
-    gm = GaussianMixture([[-1, -1], [1, 1]], [[[1, -.5], [-.5, 1]], [[0.6, -.5], [-.5, 1.4]]])
-    y = gm.generate_samples(10000)
-    plt.hist2d(y[:, 0], y[:, 1], bins=50, cmap=plt.cm.get_cmap('BuGn_r'))
-    (x1pdf, x2pdf), ypdf = gm.pdf()
-    plt.contour(x1pdf, x2pdf, ypdf)
-    plt.xlim([x1pdf[0, 0], x1pdf[-1, -1]])
-    plt.ylim([x2pdf[0, 0], x2pdf[-1, -1]])
+    GM = GaussianMixture(np.array([[-1, -1], [1, 1]]),
+                         np.array([[[1, -.5], [-.5, 1]], [[0.6, -.5], [-.5, 1.4]]]))
+    YDATA = GM.generate_samples(10000)
+    plt.hist2d(YDATA[:, 0], YDATA[:, 1], bins=50, cmap=plt.cm.get_cmap('BuGn_r'))
+    (X1PDF, X2PDF), YPDF = GM.pdf()
+    plt.contour(X1PDF, X2PDF, YPDF)
+    plt.xlim([X1PDF[0, 0], X1PDF[-1, -1]])
+    plt.ylim([X2PDF[0, 0], X2PDF[-1, -1]])
     plt.title("Histogram of data with contour plot of real PDF")
     plt.show()
