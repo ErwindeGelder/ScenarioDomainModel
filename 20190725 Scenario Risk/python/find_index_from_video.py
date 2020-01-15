8d7d8bedbee4dc1f011c583beaa1b521bfd708e4
Erwin de Gelder <erwin.degelder@tno.nl>
2020-01-15 17:44:20 +0800

    Add the option to not print the seconds and the index.

--- 20190725 Scenario Risk/python/find_index_from_video.py
+++ 20190725 Scenario Risk/python/find_index_from_video.py
@@ -4,6 +4,7 @@ Creation date: 2019 12 08
 Author(s): Erwin de Gelder
 
 Modifications:
+2020 01 13 Add the option to not print the seconds and the index.
 """
 
 import pandas as pd
@@ -26,16 +27,18 @@ def find_neighbours(value: float, data: pd.DataFrame) -> float:
     return lowerneighbour_ind
 
 
-def approx_index(minute: int, second: float, data: pd.DataFrame) -> float:
+def approx_index(minute: int, second: float, data: pd.DataFrame, verbose: bool = True) -> float:
     """ Find the index in the data at the given video time.
 
     :param minute: The minute in the video.
     :param second: The second in the video.
     :param data: The dataframe that contains the field `video_time`.
+    :param verbose: Print the seconds and the index.
     :return: The index in the data.
     """
     iframe = (minute * 60 + second) * 10
-    print("Seconds: {:.0f}".format(minute * 60 + second))
     index = find_neighbours(iframe, data)
-    print("Index: {:.2f}".format(index))
+    if verbose:
+        print("Seconds: {:.0f}".format(minute * 60 + second))
+        print("Index: {:.2f}".format(index))
     return index
