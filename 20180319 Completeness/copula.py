d668a8699431254fb24fe5eda00460f85569ea2e
Erwin de Gelder <erwin.degelder@tno.nl>
2020-02-26 10:40:33 +0100

    Add class for copula pairs.

--- 20180319 Completeness/copula.py
+++ 20180319 Completeness/copula.py
@@ -16,9 +16,12 @@ To do
 
 Modifications
 -------------
+2020 02 19 Add class for copula pairs.
+
 """
 
-from typing import List
+from copy import deepcopy
+from typing import List, Sequence, Tuple
 import numpy as np
 import scipy.special
 from fastkde import KDE, process_reshaped_data
@@ -27,6 +30,9 @@ from options import Options
 
 class CopulaOptions(Options):
     """ Options for the copula. """
+    def __init__(self, **kwargs):
+        self.kdes_bandwidths: List[float] = []
+        Options.__init__(self, **kwargs)
 
 
 class CopulaParameters(Options):
@@ -43,7 +49,10 @@ class CopulaParameters(Options):
     data_uniform: np.ndarray = np.array([])
     dim: int = 0
     kde_copula: KDE = None
-    kdes: List[KDE] = []
+
+    def __init__(self):
+        self.kdes: List[KDE] = []
+        Options.__init__(self)
 
 
 class Copula:
@@ -51,6 +60,8 @@ class Copula:
 
     Attributes:
         data: The data that is used to construct the copula.
+        options: Options for the copula, see CopulaOptions.
+        parms: Parameters of the copula, see CopulaParameters.
     """
     def __init__(self, data: np.ndarray, options: CopulaOptions = None):
         self.data = data
@@ -58,13 +69,19 @@ class Copula:
         self.parms = CopulaParameters()
 
         self.parms.dim = self.data.shape[1]
+        if not self.options.kdes_bandwidths:
+            self.options.kdes_bandwidths = [None for _ in range(self.parms.dim)]
+        self.fit()
 
     def fit(self) -> None:
         """ Fit """
         # Construct KDEs of marginal probabilities.
         self.parms.kdes = [KDE(self.data[:, i]) for i in range(self.parms.dim)]
-        for kde in self.parms.kdes:
-            kde.compute_bandwidth()
+        for kde, bandwidth in zip(self.parms.kdes, self.options.kdes_bandwidths):
+            if bandwidth is None:
+                kde.compute_bandwidth()
+            else:
+                kde.set_bandwidth(bandwidth)
 
         # Convert the data such that is in the range [0, 1].
         self.parms.data_uniform = np.array(np.zeros_like(self.data))
@@ -114,3 +131,86 @@ class Copula:
     def _copula(self, xdata: np.ndarray) -> np.ndarray:
         xdata = scipy.special.erfinv(xdata*2-1)*np.sqrt(2)
         return self.parms.kde_copula.cdf(xdata)
+
+    def cdf(self, xdata: np.ndarray) -> np.ndarray:
+        """ Compute the actual cumulative distribution.
+
+        :param xdata: The points for which the cdf is required.
+        :return: The cdf values.
+        """
+        return process_reshaped_data(xdata, self._cdf)
+
+    def _cdf(self, xdata: np.ndarray) -> np.ndarray:
+        data_uniform = np.array(np.zeros_like(xdata))
+        for i, kde in enumerate(self.parms.kdes):
+            data_uniform[:, i] = kde.cdf(xdata[:, i])
+        return self._copula(data_uniform)
+
+
+class CopulaPairsParameters(Options):
+    """ Parameters of the copula pairs.
+
+    This class contains the following attributes:
+        common_vars: List of the common variables.
+    """
+    def __init__(self):
+        self.common_vars: List[Tuple[int, int]] = []
+        self.copulas: List[Copula] = []
+        Options.__init__(self)
+
+
+class CopulaPairs:
+    """ Model the distribution with multiple pair copulas.
+
+    Attributes:
+        data: The data that is used to construct the copula.
+        pairs: The pairs of the copula.
+        options: Options for the copula, see CopulaOptions.
+        parms: Parameters of the copula pairs, see CopulaPairsParameters
+    """
+    def __init__(self, data: np.ndarray, pairs: Sequence[Tuple[int, int]],
+                 options: CopulaOptions = None):
+        self.data = data
+        if len(pairs) != (data.shape[1] - 1):
+            raise ValueError("Number of pairs must be the number of dimensions minus one.")
+        self.pairs = pairs
+        self.options = CopulaOptions() if options is None else options
+        self.parms = CopulaPairsParameters()
+        self.fit()
+
+    def fit(self) -> None:
+        """ Fit the copulas. """
+        self.parms.common_vars = []
+        for i, (parta, partb) in enumerate(self.pairs):
+            # Loop through the previous pairs and see if we have seen these variables earlier.
+            bandwidths = [None, None]
+            for copula, pair in zip(self.parms.copulas[:i], self.pairs[:i]):
+                if parta in pair:
+                    self.parms.common_vars.append((i, 0))
+                    bandwidths[0] = copula.parms.kdes[pair.index(parta)].bandwidth
+                if partb in pair:
+                    self.parms.common_vars.append((i, 1))
+                    bandwidths[1] = copula.parms.kdes[pair.index(partb)].bandwidth
+
+            # Construct the copula.
+            options = deepcopy(self.options)
+            if not options.kdes_bandwidths:
+                options.kdes_bandwidths = bandwidths
+            self.parms.copulas.append(Copula(self.data[:, [parta, partb]], options=options))
+
+    def pdf(self, xdata: np.ndarray) -> np.ndarray:
+        """ Compute the probability density function at the given points.
+
+        :param xdata: The points for which the probability density is required.
+        :return: The probability density.
+        """
+        return process_reshaped_data(xdata, self._pdf)
+
+    def _pdf(self, xdata: np.ndarray) -> np.ndarray:
+        score = np.ones(len(xdata))
+        for copula, pair in zip(self.parms.copulas, self.pairs):
+            score *= copula.pdf(xdata[:, pair])
+        for icopula, ivar in self.parms.common_vars:
+            score /= self.parms.copulas[icopula].parms.kdes[ivar].score_samples(
+                xdata[:, self.pairs[icopula][ivar]])
+        return score
