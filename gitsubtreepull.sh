a07912bf88165c6d406163edab722ff3a8c804ca
Erwin de Gelder <erwin.degelder@tno.nl>
2020-03-04 10:58:41 +0100

    Change path of stats library.

--- gitsubtreepull.sh
+++ gitsubtreepull.sh
@@ -1,3 +1,3 @@
 git subtree pull --prefix="20191113 Journal Scenario Risk" scenariorisk master
 git subtree pull --prefix="20191010 Scenario Mining" scenariomining master
-git subtree pull --prefix="20180319 Completeness" stats master
+git subtree pull --prefix="20180319 Completeness/stats" stats master
--- gitsubtreepush.sh
+++ gitsubtreepush.sh
@@ -1,3 +1,3 @@
 git subtree push --prefix="20191113 Journal Scenario Risk" scenariorisk master
 git subtree push --prefix="20191010 Scenario Mining" scenariomining master
-git subtree push --prefix="20180319 Completeness" stats master
+git subtree push --prefix="20180319 Completeness/stats" stats master
