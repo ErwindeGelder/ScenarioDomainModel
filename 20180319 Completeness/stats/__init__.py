04238fe8ea07f5334d0f9ec71ff8b5c23bf9a6ea
Erwin de Gelder <erwin.degelder@tno.nl>
2020-03-04 11:48:44 +0100

    Update imports.

--- 20180319 Completeness/stats/__init__.py
+++ 20180319 Completeness/stats/__init__.py
@@ -1,4 +1,5 @@
 """ Different scripts that are somehow related to statistics. """
 
+from .copula import Copula, CopulaPairs, CopulaOptions
 from .fastkde import KDE
 from .gaussianmixture import GaussianMixture
