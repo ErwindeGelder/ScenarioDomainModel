43e4ba08de324d29683335c119d176c46e0d1622
Erwin de Gelder <erwin.degelder@tno.nl>
2020-03-04 10:37:27 +0100

    Add stats and have a separate script for pulling.

---
+++ gitsubtreepull.sh
@@ -0,0 +1,3 @@
+git subtree pull --prefix="20191113 Journal Scenario Risk" scenariorisk master
+git subtree pull --prefix="20191010 Scenario Mining" scenariomining master
+git subtree pull --prefix="20180319 Completeness" stats master
--- gitsubtreepush.sh
+++ gitsubtreepush.sh
@@ -1,2 +1,3 @@
 git subtree push --prefix="20191113 Journal Scenario Risk" scenariorisk master
 git subtree push --prefix="20191010 Scenario Mining" scenariomining master
+git subtree push --prefix="20180319 Completeness" stats master
