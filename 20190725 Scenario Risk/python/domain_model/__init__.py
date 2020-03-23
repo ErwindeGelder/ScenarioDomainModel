ab00e0aaee30b66a171d4ad27f8983df6536c97f
Erwin de Gelder <erwin.degelder@tno.nl>
2020-03-23 21:47:16 +0100

    Make 'from_json' functions available.

--- 20190725 Scenario Risk/python/domain_model/__init__.py
+++ 20190725 Scenario Risk/python/domain_model/__init__.py
@@ -6,17 +6,19 @@ attributes.
 """
 
 # imports to make easy access possible after importing domain_model
-from .activity_category import ActivityCategory
-from .actor_category import ActorCategory, VehicleType
-from .activity import Activity, DetectedActivity, TriggeredActivity
-from .actor import Actor, EgoVehicle
+from .activity_category import ActivityCategory, activity_category_from_json
+from .actor_category import ActorCategory, VehicleType, actor_category_from_json
+from .activity import Activity, DetectedActivity, TriggeredActivity, activity_from_json
+from .actor import Actor, EgoVehicle, actor_from_json
 from .document_management import DocumentManagement
 from .event import Event
-from .model import Spline3Knots, Linear, Sinusoidal, Constant, BSplines, MultiBSplines
-from .scenario import Scenario
-from .scenario_category import ScenarioCategory
-from .state import State
-from .state_variable import StateVariable
-from .static_environment_category import StaticEnvironmentCategory, Region
-from .static_environment import StaticEnvironment
-from .tags import Tag
+from .model import Spline3Knots, Linear, Sinusoidal, Constant, BSplines, MultiBSplines, \
+    model_from_json
+from .scenario import Scenario, scenario_from_json
+from .scenario_category import ScenarioCategory, scenario_category_from_json
+from .state import State, state_from_json
+from .state_variable import StateVariable, state_variable_from_json
+from .static_environment_category import StaticEnvironmentCategory, Region, \
+    stat_env_category_from_json
+from .static_environment import StaticEnvironment, stat_env_from_json
+from .tags import Tag, tag_from_json
