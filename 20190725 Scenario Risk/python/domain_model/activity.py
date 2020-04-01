c8aef567c932c1cd2aab74b648ce38eedf8c44fe
Erwin de Gelder <erwin.degelder@tno.nl>
2020-04-01 13:39:05 +0200

    Make it possible to evaluate the model to get the state values.

--- 20190725 Scenario Risk/python/domain_model/activity.py
+++ 20190725 Scenario Risk/python/domain_model/activity.py
@@ -24,10 +24,12 @@ Modifications
 28 Feb 2019: The start of a triggered activity is at an event.
 22 May 2019: Make use of type_checking.py to shorten the initialization.
 11 Oct 2019: Update of terminology.
+29 Mar 2020: Make it possible to evaluate the model to get the state values.
 
 """
 
 
+from typing import List, Union
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
@@ -105,6 +107,25 @@ class Activity(Default):
         axes.plot(xdata, ydata, **kwargs)
         axes.set_xlabel("Time")
         axes.set_ylabel("{:s} dot".format(self.activity_category.state.name))
+
+    def get_state(self, npoints: int = 100, time: Union[np.ndarray, float, List] = None) \
+            -> np.ndarray:
+        """ Obtain the state evaluated at given time instances.
+
+        By default, the state is returned for 100 points equally distributed
+        over time. To change the number of points, the argument `npoints` can be
+        used. Alternatively, if the argument `time` is used, the state is
+        evaluated at the provided time instances.
+
+        :param npoints: Number of points for evaluating the state.
+        :param time: Time instance(s) at which the model is to be evaluated.
+        :return: Numpy array with the state.
+        """
+        if isinstance(time, List):
+            time = np.array(time)
+        elif isinstance(time, float):
+            time = np.array([time])
+        return self.activity_category.model.get_state(self.parameters, npoints, time)
 
     def get_tags(self) -> dict:
         """ Return the list of tags related to this Activity.
