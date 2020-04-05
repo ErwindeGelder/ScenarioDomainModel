144afbdcf4ad949f8aa3b04f46f495556f98539a
Erwin de Gelder <erwin.degelder@tno.nl>
2020-04-05 12:45:46 +0200

    Also add stats to scenario risk folder.

--- gitsubtreepull.sh
+++ gitsubtreepull.sh
@@ -2,3 +2,4 @@ git subtree pull --prefix="20191113 Journal Scenario Risk" scenariorisk master
 git subtree pull --prefix="20191010 Scenario Mining" scenariomining master
 git subtree pull --prefix="20180319 Completeness/stats" stats master
 git subtree pull --prefix="20200304 Test case generation/stats" stats master
+git subtree pull --prefix="20190725 Scenario Risk/python/stats" stats master
