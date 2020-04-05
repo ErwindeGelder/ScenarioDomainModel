876fb33ec648a810ac56ee076a6f6cf782b5e7cb
Erwin de Gelder <erwin.degelder@tno.nl>
2020-04-05 22:06:05 +0200

    Compile PR29

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -389,6 +389,7 @@ if __name__ == '__main__':
     compile_pr(26, other=(join('20191010 Scenario Mining', 'scenario_mining'), dict(usebiber=True)))
     compile_pr(27, other=(join('20191010 Scenario Mining', 'scenario_mining'), dict(usebiber=True)))
     compile_pr(28, usebiber=True)
+    compile_pr(29, usebiber=True)
 
     # Revisions/versions
     compile_doc(join('20180924 Completeness paper', 'completeness'), git='CompletenessPaperInit',
