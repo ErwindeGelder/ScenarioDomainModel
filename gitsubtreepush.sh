e65a6a03648410e2d5dd952c66dd424f20e5a2d7
Erwin de Gelder <erwin.degelder@tno.nl>
2020-04-05 12:31:39 +0200

    Only push stats from the completeness folder. So edits need to be made in this folder!

--- gitsubtreepush.sh
+++ gitsubtreepush.sh
@@ -1,4 +1,4 @@
 git subtree push --prefix="20191113 Journal Scenario Risk" scenariorisk master
 git subtree push --prefix="20191010 Scenario Mining" scenariomining master
 git subtree push --prefix="20180319 Completeness/stats" stats master
-git subtree push --prefix="20200304 Test case generation/stats" stats master
+
