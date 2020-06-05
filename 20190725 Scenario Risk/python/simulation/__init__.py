0c3306f5851eaf7426df135a4d1aa6ff1a2b1d11
Erwin de Gelder <erwin.degelder@tno.nl>
2020-06-05 15:20:42 +0200

    Also make Simulator available for type hinting.:

--- 20190725 Scenario Risk/python/simulation/__init__.py
+++ 20190725 Scenario Risk/python/simulation/__init__.py
@@ -5,3 +5,4 @@ from .idm import IDM, IDMParameters
 from .idmplus import IDMPlus
 from .leader_braking import LeaderBraking, LeaderBrakingParameters
 from .simulation_lead_braking import SimulationLeadBraking
+from .simulator import Simulator
