e81b4cb7fba6d433358e4d034e95f0edb189531d
Erwin de Gelder <erwin.degelder@tno.nl>
2020-06-05 15:21:32 +0200

    Have min_simulations as option. Include option of having a seed.

--- 20190725 Scenario Risk/python/simulation/simulator.py
+++ 20190725 Scenario Risk/python/simulation/simulator.py
@@ -14,27 +14,33 @@ from .fastkde import KDE
 
 class Simulator(ABC):
     """ Generic functions for a simulator. """
-    def __init__(self, tolerance=0.01, max_simulations=100):
+    def __init__(self, tolerance=0.01, min_simulations=5, max_simulations=100):
         self.tolerance = tolerance
+        self.min_simulations = min_simulations
         self.max_simulations = max_simulations
 
-    def simulation(self, parameters, plot=False):
+    def simulation(self, parameters, plot=False, seed: int = None):
         """ Run a single simulation.
 
         :param parameters: Parameters of the simulation.
         :param plot: Whether to maka a plot.
+        :param seed: Specify in order to simulate with a fixed seed.
         :return: Result of the simulation.
         """
 
-    def get_probability(self, parameters, plot=False) -> float:
+    def get_probability(self, parameters, plot=False, seed: int = None) -> float:
         """ Run multiple simulations in order to get the probability of failure.
 
         :param parameters: Parameters of the simulation.
         :param plot: Whether to make a plot.
+        :param seed: Specify in order to simulate with a fixed seed.
         :return: Result of the simulation.
         """
+        if seed is not None:
+            np.random.seed(seed)
+
         # At least run the simulation few times in order to construct a KDE.
-        n_simulations = 2
+        n_simulations = self.min_simulations
         data = np.array([self.simulation(parameters) for _ in range(n_simulations)])
         kde = KDE(data)
         kde.compute_bandwidth()
