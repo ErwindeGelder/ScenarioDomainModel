5d0f79215b0f2d07d1924aa70963dac169f31df3
Erwin de Gelder <erwin.degelder@tno.nl>
2020-06-23 16:51:14 +0200

    Add functions.

--- 20190725 Scenario Risk/python/simulation/__init__.py
+++ 20190725 Scenario Risk/python/simulation/__init__.py
@@ -1,9 +1,10 @@
 """ Different scripts used for modeling of driver behaviors. """
 
+from .eidm import EIDM, EIDMParameters
 from .hdm import HDM, HDMParameters
 from .idm import IDM, IDMParameters
-from .idmplus import IDMPlus
+from .idmplus import IDMPlus, EIDMPlus
 from .leader_braking import LeaderBraking, LeaderBrakingParameters
-from .simulation_lead_braking import SimulationLeadBraking
+from .simulation_lead_braking import SimulationLeadBraking, hdm_parameters, eidm_parameters
 from .simulation_lead_interaction import SimulationLeadInteraction
 from .simulator import Simulator
