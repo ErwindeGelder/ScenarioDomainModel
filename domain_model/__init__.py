70720772bc02850ee358550f82a9a7d71dfd0645
Erwin de Gelder <erwin.degelder@tno.nl>
2020-08-02 18:24:32 +0200

    Make the ...from_json functions available.

--- domain_model/__init__.py
+++ domain_model/__init__.py
@@ -6,16 +6,17 @@ attributes.
 """
 
 # imports to make easy access possible after importing domain_model
-from .activity_category import ActivityCategory
-from .actor_category import ActorCategory, VehicleType
-from .model import Spline3Knots, Linear, Sinusoidal, Constant
-from .activity import Activity, SetActivity, TriggeredActivity
-from .actor import Actor, EgoVehicle
-from .scenario import Scenario
-from .scenario_category import ScenarioCategory
-from .tags import Tag
-from .state_variable import StateVariable
-from .static_environment_category import StaticEnvironmentCategory, Region
-from .static_environment import StaticEnvironment
-from .event import Event
-from .state import State
+from .activity_category import ActivityCategory, activity_category_from_json
+from .actor_category import ActorCategory, VehicleType, actor_category_from_json
+from .model import Spline3Knots, Linear, Sinusoidal, Constant, model_from_json
+from .activity import Activity, SetActivity, TriggeredActivity, activity_from_json
+from .actor import Actor, EgoVehicle, actor_from_json
+from .scenario import Scenario, scenario_from_json
+from .scenario_category import ScenarioCategory, scenario_category_from_json
+from .tags import Tag, tag_from_json
+from .state_variable import StateVariable, state_variable_from_json
+from .static_environment_category import StaticEnvironmentCategory, Region, \
+    stat_env_category_from_json, region_from_json
+from .static_environment import StaticEnvironment, stat_env_from_json
+from .event import Event, event_from_json
+from .state import State, state_from_json
