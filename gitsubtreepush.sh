1dbaed774d659423c8a9bbd70e4749f0c745003a
Erwin de Gelder <erwin.degelder@tno.nl>
2020-08-10 12:08:42 +0200

    Add domain model to gitsubtree push commands.

--- gitsubtreepush.sh
+++ gitsubtreepush.sh
@@ -2,4 +2,5 @@ git subtree push --prefix="20191113 Journal Scenario Risk" scenariorisk master
 git subtree push --prefix="20191010 Scenario Mining" scenariomining master
 git subtree push --prefix="20180319 Completeness/stats" stats master
 # git subtree push --prefix="20190725 Scenario Risk/python/stats" stats master
+# git subtree push --prefix="20200729 Domain Model" domainmodel master
 
