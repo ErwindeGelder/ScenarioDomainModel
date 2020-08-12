5c1bf0b06a7f6994e2899b96233e2f2ef067b4ae
Erwin de Gelder <erwin.degelder@tno.nl>
2020-08-12 11:35:12 +0200

    Model ACC with FCW and a human modelled with HDM.

--- 20190725 Scenario Risk/python/simulation/__init__.py
+++ 20190725 Scenario Risk/python/simulation/__init__.py
@@ -1,12 +1,13 @@
 """ Different scripts used for modeling of driver behaviors. """
 
 from .acc import ACC, ACCParameters
+from .acc_hdm import ACCHDM, ACCHDMParameters
 from .eidm import EIDM, EIDMParameters
 from .hdm import HDM, HDMParameters
 from .idm import IDM, IDMParameters
 from .idmplus import IDMPlus, EIDMPlus
 from .leader_braking import LeaderBraking, LeaderBrakingParameters
 from .simulation_lead_braking import SimulationLeadBraking, hdm_lead_braking_pars, \
-    eidm_lead_braking_pars, acc_lead_braking_pars
+    eidm_lead_braking_pars, acc_lead_braking_pars, acc_hdm_lead_braking_pars
 from .simulation_lead_interaction import SimulationLeadInteraction
 from .simulator import Simulator
