9a7e6b16de8323a155477aa4bcdb787ceedcc884
Erwin de Gelder <erwin.degelder@tno.nl>
2020-08-19 13:17:18 +0200

    Make Actor a subclass of DynamicPhysicalThing.

--- 20200729 Domain Model/domain_model/actor.py
+++ 20200729 Domain Model/domain_model/actor.py
@@ -14,95 +14,46 @@ Modifications:
             desried state.
 2019 10 11: Update of terminology.
 2019 11 04: Add goals to ego vehicle.
+2020 08 19: Make Actor a subclass of DynamicPhysicalThing
 """
 
 from typing import List
-from .thing import Thing
 from .actor_category import ActorCategory, actor_category_from_json
-from .tags import Tag, tag_from_json
+from .dynamic_physical_thing import DynamicPhysicalThing
 from .state import State, state_from_json
+from .tags import Tag, tag_from_json
 from .type_checking import check_for_type, check_for_list
 
 
-class Actor(Thing):
-    """ Category of actor
+class Actor(DynamicPhysicalThing):
+    """ Actor
 
-    An actor is an agent in a scenario acting on its own behalf. "Ego vehicle"
-    and "Other Road User" are types of actors in a scenario. The actor category
-    only describes the actor in qualitative terms.
+    An actor is a dynamic physical thing that can have an intent. "Ego vehicle"
+    and "Other Road User" are types of actors in a scenario.
 
     Attributes:
-        actor_category (ActorCategory): Specifying the category to which the
-            actor belongs to.
-        initial_states (List[State]): Specifying the initial states.
-        desired_states (List[State]): Specifying the goal/objectve of the actor.
-        goal (str): A goal that cannot be formulated using the desired state.
-        name (str): A name that serves as a short description of the qualitative
-            actor.
         uid (int): A unique ID.
+        name (str): A name that serves as a short description of the actor
+            category.
         tags (List[Tag]): The tags are used to determine whether a scenario
             category comprises a scenario.
+        properties(dict): All properties of the dynamic physical thing.
+        initial_states (List[State]): Specifying the initial states.
+        desired_states (List[State]): Specifying the goal/objectve of the actor.
+        category (ActorCategory): The qualitative counterpart.
     """
-    def __init__(self, actor_category: ActorCategory, initial_states: List[State] = None,
-                 desired_states: List[State] = None, goal: str = "", **kwargs):
+    def __init__(self, actor_category: ActorCategory, desired_states: List[State] = None,
+                 initial_states: List[State] = None, properties: dict = None, **kwargs):
         # Check the types of the inputs
         check_for_type("actor_category", actor_category, ActorCategory)
-        check_for_list("initial_states", initial_states, State)
         check_for_list("desired_states", desired_states, State)
-        check_for_type("goal", goal, str)
-        if initial_states is None:
-            initial_states = []
-        if desired_states is None:
-            desired_states = []
 
-        Thing.__init__(self, **kwargs)
-        self.actor_category = actor_category  # type: ActorCategory
-        self.initial_states = initial_states  # type: List[State]
-        self.desired_states = desired_states  # type: List[State]
-        self.goal = goal                      # type: str
-
-    def get_tags(self) -> dict:
-        """ Return the list of tags related to this Actor.
-
-        It returns the tags associated to this Actor and the tags associated
-        with the ActorCategory.
-
-        :return: List of tags.
-        """
-        tags = self.tags
-        tags += self.actor_category.get_tags()
-        return tags
+        DynamicPhysicalThing.__init__(self, actor_category, initial_states, properties, **kwargs)
+        self.desired_states = [] if desired_states is None else desired_states  # type: List[State]
 
     def to_json(self) -> dict:
-        """ Get JSON code of object.
-
-        For storing scenarios into the database, the scenarios need to be
-        converted to JSON. This method converts the attributes of Actor to JSON.
-
-        :return: dictionary that can be converted to a json file.
-        """
-        actor = Thing.to_json(self)
-        actor["actor_category"] = {"name": self.actor_category.name,
-                                   "uid": self.actor_category.uid}
-        actor["initial_states"] = [initial_state.to_json() for initial_state in self.initial_states]
+        actor = DynamicPhysicalThing.to_json(self)
         actor["desired_states"] = [desired_state.to_json() for desired_state in self.desired_states]
-        return actor
-
-    def to_json_full(self) -> dict:
-        """ Get full JSON code of object.
-
-        As opposed to the to_json() method, this method can be used to fully
-        construct the object. It might be that the to_json() code links to its
-        attributes with only a unique id and name. With this information the
-        corresponding object can be looked up into the database. This method
-        returns all information, which is not meant for the database, but can be
-        used instead for describing a scenario without the need of referring to
-        the database.
-
-        :return: dictionary that can be converted to a json file
-        """
-        actor = self.to_json()
-        actor["actor_category"] = self.actor_category.to_json_full()
         return actor
 
 
@@ -119,12 +70,13 @@ def actor_from_json(json: dict, actor_category: ActorCategory = None) -> Actor:
     :return: Actor object.
     """
     if actor_category is None:
-        actor_category = actor_category_from_json(json["actor_category"])
+        actor_category = actor_category_from_json(json["category"])
     initial_states = [state_from_json(state) for state in json["initial_states"]]
     derired_states = [state_from_json(state) for state in json["desired_states"]]
     actor = Actor(actor_category,
                   initial_states=initial_states,
                   desired_states=derired_states,
+                  properties=json["properties"],
                   name=json["name"],
                   uid=int(json["id"]),
                   tags=[tag_from_json(tag) for tag in json["tag"]])
