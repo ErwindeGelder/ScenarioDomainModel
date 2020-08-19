756fc072f43543bbbe3ec40804e46d593a3cfca0
Erwin de Gelder <erwin.degelder@tno.nl>
2020-08-19 12:30:29 +0200

    Add class 'PhysicalThing'.

---
+++ domain_model/physical_thing.py
@@ -0,0 +1,39 @@
+""" Class PhysicalThing
+
+Creation date: 2020 08 19
+Author(s): Erwin de Gelder
+
+Modifications:
+"""
+
+from abc import abstractmethod
+from .quantitative_thing import QuantitativeThing
+from .type_checking import check_for_type
+
+
+class PhysicalThing(QuantitativeThing):
+    """ Class for modeling all physical things (both static and dynamic)
+
+    A PhysicalThing is used to quantitatively describe a physical thing. This is
+    an abstract class, so it is not possible to instantiate objects from this
+    class.
+
+    Attributes:
+        uid (int): A unique ID.
+        name (str): A name that serves as a short description of the actor
+            category.
+        tags (List[Tag]): The tags are used to determine whether a scenario
+            category comprises a scenario.
+        properties(dict): All properties of the physical thing.
+    """
+    @abstractmethod
+    def __init__(self, properties: dict = None, **kwargs):
+        check_for_type("properties", properties, dict)
+
+        QuantitativeThing.__init__(self, **kwargs)
+        self.properties = dict() if properties is None else properties
+
+    def to_json(self) -> dict:
+        thing = QuantitativeThing.to_json(self)
+        thing["properties"] = self.properties
+        return thing
