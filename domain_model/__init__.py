2abe1c086181f884411f7d48bb52c03a1e3fca4d
Erwin de Gelder <erwin.degelder@tno.nl>
2020-10-13 08:47:44 +0200

    Remove static/dynamic physical things.

--- domain_model/__init__.py
+++ domain_model/__init__.py
@@ -10,17 +10,13 @@ from .activity import Activity, activity_from_json
 from .activity_category import ActivityCategory, activity_category_from_json
 from .actor import Actor, EgoVehicle, actor_from_json
 from .actor_category import ActorCategory, VehicleType, actor_category_from_json
-from .dynamic_physical_thing import DynamicPhysicalThing, dynamic_physical_thing_from_json
-from .dynamic_physical_thing_category import DynamicPhysicalThingCategory, \
-    dynamic_physical_thing_category_from_json
 from .event import Event, event_from_json
 from .model import Constant, Linear, Spline3Knots, Sinusoidal, Splines, model_from_json
+from .physical_thing import PhysicalThing, physical_thing_from_json
+from .physical_thing_category import PhysicalThingCategory, physical_thing_category_from_json
 from .scenario import Scenario, scenario_from_json
 from .scenario_category import ScenarioCategory, scenario_category_from_json
 from .state import State, state_from_json
 from .state_variable import StateVariable, state_variable_from_json
-from .static_physical_thing import StaticPhysicalThing, static_physical_thing_from_json
-from .static_physical_thing_category import StaticPhysicalThingCategory, \
-    static_physical_thing_category_from_json
 from .tags import Tag, tag_from_json
 from .thing import DMObjects, get_empty_dm_object
