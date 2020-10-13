e690ae57c2a326b40623712b2b9bd683c0f557fb
Erwin de Gelder <erwin.degelder@tno.nl>
2020-10-13 08:46:33 +0200

    From now on, PhysicalThing is not an abstract class.

--- domain_model/physical_thing.py
+++ domain_model/physical_thing.py
@@ -5,19 +5,20 @@ Author(s): Erwin de Gelder
 
 Modifications:
 2020 08 22: Add function to obtain properties from a dictionary.
+2020 10 12: From now on, PhysicalThing is not an abstract class.
 """
 
-from abc import abstractmethod
+from typing import Union
+from .physical_thing_category import PhysicalThingCategory, _physical_thing_category_from_json
 from .quantitative_thing import QuantitativeThing, _quantitative_thing_props_from_json
+from .thing import DMObjects, _attributes_from_json, _object_from_json
 from .type_checking import check_for_type
 
 
 class PhysicalThing(QuantitativeThing):
     """ Class for modeling all physical things (both static and dynamic)
 
-    A PhysicalThing is used to quantitatively describe a physical thing. This is
-    an abstract class, so it is not possible to instantiate objects from this
-    class.
+    A PhysicalThing is used to quantitatively describe a physical thing.
 
     Attributes:
         uid (int): A unique ID.
@@ -25,24 +26,65 @@ class PhysicalThing(QuantitativeThing):
             thing.
         tags (List[Tag]): The tags are used to determine whether a scenario
             category comprises a scenario.
+        category (PhysicalThingCategory): The qualitative counterpart.
         properties(dict): All properties of the physical thing.
     """
-    @abstractmethod
-    def __init__(self, properties: dict = None, **kwargs):
+    def __init__(self, category: PhysicalThingCategory, properties: dict = None, **kwargs):
         if properties is None:
             properties = dict()
+        check_for_type("category", category, PhysicalThingCategory)
         check_for_type("properties", properties, dict)
 
         QuantitativeThing.__init__(self, **kwargs)
+        self.category = category  # type: PhysicalThingCategory
         self.properties = dict() if properties is None else properties
+
+    def get_tags(self) -> dict:
+        return self.tags + self.category.get_tags()
 
     def to_json(self) -> dict:
         thing = QuantitativeThing.to_json(self)
         thing["properties"] = self.properties
+        thing["category"] = {"name": self.category.name, "uid": self.category.uid}
+        return thing
+
+    def to_json_full(self) -> dict:
+        thing = self.to_json()
+        thing["category"] = self.category.to_json_full()
         return thing
 
 
-def _physical_thing_props_from_json(json: dict) -> dict:
-    props = dict(properties=json["properties"])
-    props.update(_quantitative_thing_props_from_json(json))
+def _physical_thing_props_from_json(json: dict, attribute_objects: DMObjects,
+                                    category: Union[PhysicalThingCategory, bool] = None) -> dict:
+    props = _quantitative_thing_props_from_json(json)
+    props["properties"] = json["properties"]
+    if category is not False:
+        props.update(_attributes_from_json(json, attribute_objects,
+                                           dict(category=(_physical_thing_category_from_json,
+                                                          "physical_thing_category")),
+                                           category=category))
     return props
+
+
+def _physical_thing_from_json(json: dict, attribute_objects: DMObjects,
+                              category: PhysicalThingCategory = None) -> PhysicalThing:
+    return PhysicalThing(**_physical_thing_props_from_json(json, attribute_objects, category))
+
+
+def physical_thing_from_json(json: dict, attribute_objects: DMObjects = None,
+                             category: PhysicalThingCategory = None) -> PhysicalThing:
+    """ Get PhysicalThing object from JSON code
+
+    It is assumed that all the attributes are fully defined. Hence, the
+    PhysicalThingCategory needs to be fully defined instead of only the unique
+    ID. Alternatively, the PhysicalThingCategory can be passed as optional
+    argument. In that case, the PhysicalThingCategory does not need to be
+    defined in the JSON code.
+
+    :param json: JSON code of PhysicalThing.
+    :param attribute_objects: A structure for storing all objects (optional).
+    :param category: If given, it will not be based on the JSON code.
+    :return: PhysicalThing object.
+    """
+    return _object_from_json(json, _physical_thing_from_json, "physical_thing", attribute_objects,
+                             category=category)
