9fd5e1ba31e558b73a16c8e0e93cb795a03cd514
Erwin de Gelder <erwin.degelder@tno.nl>
2020-10-14 11:11:41 +0200

    Add functionality to save interaction.

--- 20190725 Scenario Risk/python/load_interactions.py
+++ 20190725 Scenario Risk/python/load_interactions.py
@@ -4,6 +4,7 @@ Creation date: 2020 06 10
 Author(s): Jeroen Manders, Erwin de Gelder
 
 Modifications:
+2020 10 13: Add functionality to save interaction.
 """
 
 # Standard Library Imports
@@ -19,15 +20,11 @@ from tqdm import tqdm
 # Local Application/Library Specific Imports
 
 
-def load_interaction(file_path):
+def load_interaction(file_path: str) -> dict:
     """ Loads a single interaction hdf5.
 
-    Args:
-        file_path: path of interaction hdf5 file
-
-    Returns:
-        interaction: dict of interaction
-
+    :param file_path: path of interaction hdf5 file.
+    :Returns: dict of interaction.
     """
     with pd.HDFStore(file_path, mode="r") as hdf_store:
         interaction_dict = dict(id=os.path.splitext(os.path.basename(file_path))[0],
@@ -35,6 +32,19 @@ def load_interaction(file_path):
                                 vehicle_front=hdf_store.get("vehicle_front"),
                                 interaction=hdf_store.get("interaction"))
     return interaction_dict
+
+
+def save_interaction(interaction_dict: dict, folder: str = os.path.join("data", "8_interactions")) \
+        -> None:
+    """ Store the interaction data in an HDF5 file.
+
+    :param interaction_dict: The dictionary with the interaction data.
+    :param folder: Folder to store the file in (default: data/8_interactions).
+    """
+    filename = os.path.join(folder, "{:s}.hdf5".format(interaction_dict["id"]))
+    with pd.HDFStore(filename, mode="w") as hdf_store:
+        for key in ["vehicle_rear", "vehicle_front", "interaction"]:
+            hdf_store.put(key, interaction_dict[key], format='table')
 
 
 if __name__ == "__main__":
