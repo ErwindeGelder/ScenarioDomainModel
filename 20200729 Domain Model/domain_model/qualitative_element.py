55f216ff60b4ada09b5b24624fb9f7b76432805e
Erwin de Gelder <erwin.degelder@tno.nl>
2020-10-25 11:34:40 +0100

    Change name from QualitativeThing to QualitativeElement.

--- 20200729 Domain Model/domain_model/qualitative_element.py
+++ 20200729 Domain Model/domain_model/qualitative_element.py
@@ -1,23 +1,25 @@
-""" Class QualitativeThing
+""" Class QualitativeElement
 
 Creation date: 2020 08 15
 Author(s): Erwin de Gelder
 
 Modifications:
 2020 08 25: Add function to obtain properties from a dictionary.
+2020 10 25: Change name from QualitativeThing to QualitativeElement.
 """
 
 from abc import abstractmethod
-from .scenario_element import ScenarioElement, _thing_props_from_json
+from .scenario_element import ScenarioElement, _scenario_element_props_from_json
 from .type_checking import check_for_type
 
 
-class QualitativeThing(ScenarioElement):
+class QualitativeElement(ScenarioElement):
     """ ScenarioElement that is used for most qualitative classes.
 
-    Next to the attributes of ScenarioElement, a QualitativeThing also has a description
-    that can be used to qualitatively describe the thing. This is an abstract
-    class, so it is not possible to instantiate objects from this class.
+    Next to the attributes of ScenarioElement, a QualitativeElement also has a
+    description that can be used to qualitatively describe the thing. This is an
+    abstract class, so it is not possible to instantiate objects from this
+    class.
 
     Attributes:
         uid (int): A unique ID.
@@ -41,7 +43,7 @@ class QualitativeThing(ScenarioElement):
         return thing
 
 
-def _qualitative_thing_props_from_json(json: dict) -> dict:
+def _qualitative_element_props_from_json(json: dict) -> dict:
     props = dict(description=json["description"])
-    props.update(_thing_props_from_json(json))
+    props.update(_scenario_element_props_from_json(json))
     return props
