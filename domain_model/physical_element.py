f588ee9711bb17636302848854d11e93bcb5ddf5
Erwin de Gelder <erwin.degelder@tno.nl>
2020-10-25 11:36:02 +0100

    Change PhysicalThing to PhysicalElement.

--- domain_model/physical_element.py
+++ domain_model/physical_element.py
@@ -1,4 +1,4 @@
-""" Class PhysicalThing
+""" Class PhysicalElement
 
 Creation date: 2020 08 19
 Author(s): Erwin de Gelder
@@ -6,28 +6,30 @@ Author(s): Erwin de Gelder
 Modifications:
 2020 08 22: Add function to obtain properties from a dictionary.
 2020 10 12: From now on, PhysicalThing is not an abstract class.
+2020 10 25: Change PhysicalThing to PhysicalElement.
 """
 
 from typing import Union
 from .physical_element_category import PhysicalElementCategory, _physical_element_category_from_json
-from .quantitative_element import QuantitativeThing, _quantitative_thing_props_from_json
+from .quantitative_element import QuantitativeElement, _quantitative_element_props_from_json
 from .scenario_element import DMObjects, _attributes_from_json, _object_from_json
 from .type_checking import check_for_type
 
 
-class PhysicalThing(QuantitativeThing):
-    """ Class for modeling all physical things (both static and dynamic)
+class PhysicalElement(QuantitativeElement):
+    """ Class for modeling all physical elements (both static and dynamic)
 
-    A PhysicalThing is used to quantitatively describe a physical thing.
+    A PhysicalElement is used to quantitatively describe an object that exists
+    in the three-dimensional space.
 
     Attributes:
         uid (int): A unique ID.
         name (str): A name that serves as a short description of the physical
-            thing.
+            element.
         tags (List[Tag]): The tags are used to determine whether a scenario
             category comprises a scenario.
         category (PhysicalElementCategory): The qualitative counterpart.
-        properties(dict): All properties of the physical thing.
+        properties(dict): All properties of the physical element.
     """
     def __init__(self, category: PhysicalElementCategory, properties: dict = None, **kwargs):
         if properties is None:
@@ -35,7 +37,7 @@ class PhysicalThing(QuantitativeThing):
         check_for_type("category", category, PhysicalElementCategory)
         check_for_type("properties", properties, dict)
 
-        QuantitativeThing.__init__(self, **kwargs)
+        QuantitativeElement.__init__(self, **kwargs)
         self.category = category  # type: PhysicalElementCategory
         self.properties = dict() if properties is None else properties
 
@@ -43,7 +45,7 @@ class PhysicalThing(QuantitativeThing):
         return self.tags + self.category.get_tags()
 
     def to_json(self) -> dict:
-        thing = QuantitativeThing.to_json(self)
+        thing = QuantitativeElement.to_json(self)
         thing["properties"] = self.properties
         thing["category"] = {"name": self.category.name, "uid": self.category.uid}
         return thing
@@ -54,26 +56,27 @@ class PhysicalThing(QuantitativeThing):
         return thing
 
 
-def _physical_thing_props_from_json(json: dict, attribute_objects: DMObjects,
-                                    category: Union[PhysicalElementCategory, bool] = None) -> dict:
-    props = _quantitative_thing_props_from_json(json)
+def _physical_element_props_from_json(json: dict, attribute_objects: DMObjects,
+                                      category: Union[PhysicalElementCategory, bool] = None) \
+        -> dict:
+    props = _quantitative_element_props_from_json(json)
     props["properties"] = json["properties"]
     if category is not False:
         props.update(_attributes_from_json(json, attribute_objects,
                                            dict(category=(_physical_element_category_from_json,
-                                                          "physical_thing_category")),
+                                                          "physical_element_category")),
                                            category=category))
     return props
 
 
-def _physical_thing_from_json(json: dict, attribute_objects: DMObjects,
-                              category: PhysicalElementCategory = None) -> PhysicalThing:
-    return PhysicalThing(**_physical_thing_props_from_json(json, attribute_objects, category))
+def _physical_element_from_json(json: dict, attribute_objects: DMObjects,
+                                category: PhysicalElementCategory = None) -> PhysicalElement:
+    return PhysicalElement(**_physical_element_props_from_json(json, attribute_objects, category))
 
 
-def physical_thing_from_json(json: dict, attribute_objects: DMObjects = None,
-                             category: PhysicalElementCategory = None) -> PhysicalThing:
-    """ Get PhysicalThing object from JSON code
+def physical_element_from_json(json: dict, attribute_objects: DMObjects = None,
+                               category: PhysicalElementCategory = None) -> PhysicalElement:
+    """ Get PhysicalElement object from JSON code
 
     It is assumed that all the attributes are fully defined. Hence, the
     PhysicalElementCategory needs to be fully defined instead of only the unique
@@ -81,10 +84,10 @@ def physical_thing_from_json(json: dict, attribute_objects: DMObjects = None,
     argument. In that case, the PhysicalElementCategory does not need to be
     defined in the JSON code.
 
-    :param json: JSON code of PhysicalThing.
+    :param json: JSON code of PhysicalElement.
     :param attribute_objects: A structure for storing all objects (optional).
     :param category: If given, it will not be based on the JSON code.
-    :return: PhysicalThing object.
+    :return: PhysicalElement object.
     """
-    return _object_from_json(json, _physical_thing_from_json, "physical_thing", attribute_objects,
-                             category=category)
+    return _object_from_json(json, _physical_element_from_json, "physical_element",
+                             attribute_objects, category=category)
