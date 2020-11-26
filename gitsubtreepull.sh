c6d220bd1870919b42c8219ce62af398f02a7c25
Erwin de Gelder <erwin.degelder@tno.nl>
2020-11-26 16:22:22 +0100

    Add anomaly detection subtree

--- gitsubtreepull.sh
+++ gitsubtreepull.sh
@@ -5,3 +5,4 @@ git subtree pull --prefix="20200304 Test case generation/stats" stats master
 git subtree pull --prefix="20190725 Scenario Risk/python/stats" stats master
 # git subtree pull --prefix="20190725 Scenario Risk/python/domain_model" domainmodel master
 # git subtree pull --prefix="20201126 Risk Metric" riskmetric master
+# git subtree pull --prefix="20201126 Anomaly Detection" anomalydetection master
--- gitsubtreepush.sh
+++ gitsubtreepush.sh
@@ -4,4 +4,4 @@ git subtree push --prefix="20180319 Completeness/stats" stats master
 # git subtree push --prefix="20190725 Scenario Risk/python/stats" stats master
 # git subtree push --prefix="20200729 Domain Model" domainmodel master
 # git subtree push --prefix="20201126 Risk Metric" riskmetric master
-
+# git subtree push --prefix="20201126 Anomaly Detection" anomalydetection master
