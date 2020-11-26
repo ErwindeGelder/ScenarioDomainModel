5db2e4ab70106cd4cb7dbdda59143954f08cb420
Erwin de Gelder <erwin.degelder@tno.nl>
2020-11-26 16:19:00 +0100

    Add risk metric subtree

--- gitsubtreepull.sh
+++ gitsubtreepull.sh
@@ -4,3 +4,4 @@ git subtree pull --prefix="20180319 Completeness/stats" stats master
 git subtree pull --prefix="20200304 Test case generation/stats" stats master
 git subtree pull --prefix="20190725 Scenario Risk/python/stats" stats master
 # git subtree pull --prefix="20190725 Scenario Risk/python/domain_model" domainmodel master
+# git subtree pull --prefix="20201126 Risk Metric" riskmetric master
--- gitsubtreepush.sh
+++ gitsubtreepush.sh
@@ -3,4 +3,5 @@ git subtree push --prefix="20191010 Scenario Mining" scenariomining master
 git subtree push --prefix="20180319 Completeness/stats" stats master
 # git subtree push --prefix="20190725 Scenario Risk/python/stats" stats master
 # git subtree push --prefix="20200729 Domain Model" domainmodel master
+# git subtree push --prefix="20201126 Risk Metric" riskmetric master
 
