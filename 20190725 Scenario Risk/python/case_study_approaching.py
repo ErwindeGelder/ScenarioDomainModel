ba63a436681454dd6363d11cd84ead38a5eb7749
Erwin de Gelder <erwin.degelder@tno.nl>
2020-12-09 15:32:15 +0100

    Use IDM+ instead of HDM.

--- 20190725 Scenario Risk/python/case_study_approaching.py
+++ 20190725 Scenario Risk/python/case_study_approaching.py
@@ -10,11 +10,11 @@ import argparse
 import os
 from typing import List, Union
 import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 import numpy as np
-from databaseemulator import DataBaseEmulator
-from domain_model import StateVariable
-from simulation import SimulationApproaching, HDM, ACC, ACCHDM, hdm_approaching_pars, \
-    acc_approaching_pars, acc_hdm_approaching_pars
+from domain_model import StateVariable, DocumentManagement
+from simulation import SimulationApproaching, IDMPlus, ACC, idm_approaching_pars, \
+    acc_approaching_pars
 from case_study import CaseStudy, CaseStudyOptions, default_process_result
 
 
@@ -32,14 +32,14 @@ def parameters_approaching() -> np.ndarray:
 
     :return: The numpy array with the parameters.
     """
-    data = DataBaseEmulator(os.path.join("data", "5_scenarios", "approaching_vehicle.json"))
+    data = DocumentManagement(os.path.join("data", "5_scenarios", "approaching_vehicle2.json"))
     pars = []
-    for i in range(len(data.collections["scenario"])):
-        scenario = data.get_ordered_item("scenario", i)
+    for key in data.collections["scenario"]:
+        scenario = data.get_item("scenario", key)
         vego = scenario.get_state(scenario.get_actor_by_name("ego vehicle"), StateVariable.SPEED,
-                                  scenario.time["start"])
+                                  scenario.get_tstart())
         vtarget = scenario.get_state(scenario.get_actor_by_name("target vehicle"),
-                                     StateVariable.LON_TARGET, scenario.time["start"])[0]
+                                     StateVariable.LON_TARGET, scenario.get_tstart())[0]
         if vego > vtarget:
             pars.append([vego, vtarget/vego])
 
@@ -56,42 +56,43 @@ def check_validity_approaching(par: Union[List, np.ndarray]) -> bool:
     if par[0] <= 0:
         return False
 
-    # Target speed must not be negative and less than ego speed.
-    if par[1] < 0 or par[1] >= par[0]:
+    # Relatiev speed of target speed must not be negative and less than 1.
+    if par[1] < 0 or par[1] >= 1:
         return False
 
     return True
 
 
-def plot_result(case_study: CaseStudy, title: str = ""):
+def plot_result(case_study: CaseStudy, title: str = "") -> Figure:
     """ Plot some results. """
 
-    _, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 5))
+    figure, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 5))
     ax1.scatter(case_study.df_mc["vego"], case_study.df_mc["ratio_vtar_vego"],
                 c=case_study.df_mc["kpi"])
     ax1.set_xlabel("Speed ego vehicle [m/s]")
     ax1.set_ylabel("Ratio target/ego speed")
     ax1.set_title("{:s}\nMonte Carlo, yellow = collision".format(title))
-    xlim = ax1.get_xlim()
-    ylim = ax1.get_ylim()
-
-    ax2.scatter(case_study.df_is["vego"], case_study.df_is["ratio_vtar_vego"],
-                c=case_study.df_is["kpi"])
-    ax2.set_xlabel("Speed ego vehicle [m/s]")
-    ax2.set_ylabel("Ratio target/ego speed")
-    ax2.set_title("{:s}\nImportance sampling, yellow = collision".format(title))
-    ax2.set_xlim(xlim)
-    ax2.set_ylim(ylim)
-
-    values, indices = np.unique(case_study.df_grid[['vego', 'ratio_vtar_vego']].values, axis=0,
-                                return_inverse=True)
-    collisions = np.zeros(len(values))
-    for i in range(len(values)):
-        collisions[i] = np.sum(case_study.df_grid.loc[indices == i, "kpi"])
-    ax3.scatter(values[:, 0], values[:, 1], c=collisions)
-    ax3.set_xlabel("Speed ego vehicle [m/s]")
-    ax3.set_ylabel("Ratio target/ego speed")
-    ax3.set_title("{:s}\nyellower = more collision (total={:.0f})".format(title, sum(collisions)))
+    # xlim = ax1.get_xlim()
+    # ylim = ax1.get_ylim()
+    #
+    # ax2.scatter(case_study.df_is["vego"], case_study.df_is["ratio_vtar_vego"],
+    #             c=case_study.df_is["kpi"])
+    # ax2.set_xlabel("Speed ego vehicle [m/s]")
+    # ax2.set_ylabel("Ratio target/ego speed")
+    # ax2.set_title("{:s}\nImportance sampling, yellow = collision".format(title))
+    # ax2.set_xlim(xlim)
+    # ax2.set_ylim(ylim)
+    #
+    # values, indices = np.unique(case_study.df_grid[['vego', 'ratio_vtar_vego']].values, axis=0,
+    #                             return_inverse=True)
+    # collisions = np.zeros(len(values))
+    # for i in range(len(values)):
+    #     collisions[i] = np.sum(case_study.df_grid.loc[indices == i, "kpi"])
+    # ax3.scatter(values[:, 0], values[:, 1], c=collisions)
+    # ax3.set_xlabel("Speed ego vehicle [m/s]")
+    # ax3.set_ylabel("Ratio target/ego speed")
+    # ax3.set_title("{:s}\nyellower = more collision (total={:.0f})".format(title, sum(collisions)))
+    return figure
 
 
 if __name__ == "__main__":
@@ -100,19 +101,22 @@ if __name__ == "__main__":
                         func_validity_parameters=check_validity_approaching,
                         func_process_result=default_process_result,
                         parameters=["vego", "ratio_vtar_vego"],
+                        default_parameters=dict(amin=-3),
+                        do_importance_sampling_mcmc=False,
+                        do_importance_sampling_direct=False,
                         init_par_is=[45, 0],
                         mcmc_step=np.array([4.0, 0.1]),
                         nmc=1000,
                         grid_parameters=[np.linspace(10, 50, 17),
                                          np.linspace(0.1, 1, 19)])
 
-    print("HDM:")
-    HDM_PARS = dict(filename_prefix="approaching_hdm",
-                    simulator=SimulationApproaching(follower=HDM(),
-                                                    follower_parameters=hdm_approaching_pars))
-    HDM_PARS.update(DEFAULT_PARS)
-    CASE_STUDY = CaseStudy(CaseStudyOptions(**HDM_PARS))
-    plot_result(CASE_STUDY, "HDM")
+    print("IDM+:")
+    IDM_PARS = dict(filename_prefix="approaching_idmplus",
+                    simulator=SimulationApproaching(follower=IDMPlus(),
+                                                    follower_parameters=idm_approaching_pars))
+    IDM_PARS.update(DEFAULT_PARS)
+    CASE_STUDY = CaseStudy(CaseStudyOptions(**IDM_PARS))
+    plot_result(CASE_STUDY, "IDM").savefig(os.path.join("figs", "approaching_idmplus.png"))
 
     print()
     print("ACC:")
@@ -121,14 +125,14 @@ if __name__ == "__main__":
                                                     follower_parameters=acc_approaching_pars))
     ACC_PARS.update(DEFAULT_PARS)
     CASE_STUDY = CaseStudy(CaseStudyOptions(**ACC_PARS))
-    plot_result(CASE_STUDY, "ACC")
+    plot_result(CASE_STUDY, "ACC").savefig(os.path.join("figs", "approaching_acc.png"))
 
-    print()
-    print("ACC, FCW, and HDM:")
-    ACC_HDM_PARS = dict(filename_prefix="approaching_acchdm",
-                        simulator=SimulationApproaching(
-                            follower=ACCHDM(), follower_parameters=acc_hdm_approaching_pars))
-    ACC_HDM_PARS.update(DEFAULT_PARS)
-    CASE_STUDY = CaseStudy(CaseStudyOptions(**ACC_HDM_PARS))
-    plot_result(CASE_STUDY, "ACC, FCW, and HDM")
+    # print()
+    # print("ACC, FCW, and HDM:")
+    # ACC_HDM_PARS = dict(filename_prefix="approaching_acchdm",
+    #                     simulator=SimulationApproaching(
+    #                         follower=ACCHDM(), follower_parameters=acc_hdm_approaching_pars))
+    # ACC_HDM_PARS.update(DEFAULT_PARS)
+    # CASE_STUDY = CaseStudy(CaseStudyOptions(**ACC_HDM_PARS))
+    # plot_result(CASE_STUDY, "ACC, FCW, and HDM").savefig(os.path.join("figs", "approaching_acchdm.png"))
     plt.show()
