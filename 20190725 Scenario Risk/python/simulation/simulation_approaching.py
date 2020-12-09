238bb983e2a32e3bbc3f448a60f4d2ff8667735a
Erwin de Gelder <erwin.degelder@tno.nl>
2020-12-09 15:29:47 +0100

    Adding idm_approaching_pars.

--- 20190725 Scenario Risk/python/simulation/simulation_approaching.py
+++ 20190725 Scenario Risk/python/simulation/simulation_approaching.py
@@ -4,8 +4,10 @@ Creation date: 2020 08 13
 Author(s): Erwin de Gelder
 
 Modifications:
+2020 12 09: Adding idm_approaching_pars.
 """
 
+import numpy as np
 from .acc import ACCParameters
 from .acc_hdm import ACCHDMParameters
 from .hdm import HDMParameters
@@ -15,7 +17,7 @@ from .leader_braking import LeaderBraking, LeaderBrakingParameters
 from .simulation_longitudinal import SimulationLongitudinal
 
 
-INIT_POSITION_FOLLOWER = -300
+INIT_POSITION_FOLLOWER = -200
 
 
 def hdm_approaching_pars(**kwargs):
@@ -24,6 +26,7 @@ def hdm_approaching_pars(**kwargs):
     :return: Parameter object that can be passed via init_simulation.
     """
     steptime = 0.01
+    amin = kwargs["amin"] if "amin" in kwargs else -10
     parameters = HDMParameters(model=IDMPlus(), speed_std=0.05, tau=20, rttc=0.01,
                                timestep=steptime,
                                parms_model=IDMParameters(speed=kwargs["vego"],
@@ -34,7 +37,8 @@ def hdm_approaching_pars(**kwargs):
                                                          thw=1.1,
                                                          safety_distance=2,
                                                          a_acc=1,
-                                                         b_acc=1.5))
+                                                         b_acc=1.5,
+                                                         amin=amin))
     return parameters
 
 
@@ -43,10 +47,12 @@ def acc_approaching_pars(**kwargs):
 
     :return: Parameter object that can be passed via init_simulation.
     """
+    amin = kwargs["amin"] if "amin" in kwargs else -10
     return ACCParameters(speed=kwargs["vego"],
                          init_speed=kwargs["vego"],
                          init_position=INIT_POSITION_FOLLOWER,
-                         n_reaction=0)
+                         n_reaction=0,
+                         amin=amin)
 
 
 def acc_hdm_approaching_pars(**kwargs):
@@ -59,6 +65,30 @@ def acc_hdm_approaching_pars(**kwargs):
                             init_position=INIT_POSITION_FOLLOWER,
                             n_reaction=0,
                             driver_parms=hdm_approaching_pars(**kwargs))
+
+
+def idm_approaching_pars(**kwargs):
+    """ Define the parameters for the IDM model.
+
+    The reaction time is sampled from the lognormal distribution mentioned in
+    Wang & Stamatiadis (2014).
+
+    :param kwargs: Parameter object that can be passed via init_simulation.
+    """
+    reactiontime = np.random.lognormal(np.log(.92**2/np.sqrt(.92**2+.28**2)),
+                                       np.sqrt(np.log(1+.28**2/.92**2)))
+    steptime = 0.01
+    amin = kwargs["amin"] if "amin" in kwargs else -10
+    return IDMParameters(speed=kwargs["vego"],
+                         init_speed=kwargs["vego"],
+                         init_position=INIT_POSITION_FOLLOWER,
+                         timestep=steptime,
+                         n_reaction=int(reactiontime/steptime),
+                         thw=1.1,
+                         safety_distance=2,
+                         a_acc=1,
+                         b_acc=1.5,
+                         amin=amin)
 
 
 class SimulationApproaching(SimulationLongitudinal):
