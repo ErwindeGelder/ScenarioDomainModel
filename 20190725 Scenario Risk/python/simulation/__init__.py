e98ef42527abe115f9b5d6aafe3c5abe4b09fabb
Erwin de Gelder <erwin.degelder@tno.nl>
2020-12-09 15:31:56 +0100

    Adding idm_approaching_pars.

--- 20190725 Scenario Risk/python/simulation/__init__.py
+++ 20190725 Scenario Risk/python/simulation/__init__.py
@@ -10,7 +10,7 @@ from .leader_braking import LeaderBraking, LeaderBrakingParameters
 from .leader_interaction import LeaderInteraction, LeaderInteractionParameters
 from .rtbm import RTBM, RTBMParameters
 from .simulation_approaching import SimulationApproaching, hdm_approaching_pars, \
-    acc_approaching_pars, acc_hdm_approaching_pars
+    acc_approaching_pars, acc_hdm_approaching_pars, idm_approaching_pars
 from .simulation_cutin import SimulationCutIn, hdm_cutin_pars, acc_cutin_pars, acc_hdm_cutin_pars
 from .simulation_lead_braking import SimulationLeadBraking, hdm_lead_braking_pars, \
     eidm_lead_braking_pars, acc_lead_braking_pars, acc_hdm_lead_braking_pars
