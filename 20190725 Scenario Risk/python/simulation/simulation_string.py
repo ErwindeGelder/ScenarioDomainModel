33f20c98321d98e45633c8f4f819e020e95b44dc
Erwin de Gelder <erwin.degelder@tno.nl>
2021-01-08 10:36:18 +0100

    Also stop if there is an impact. Return minimal TTC if no collision.

--- 20190725 Scenario Risk/python/simulation/simulation_string.py
+++ 20190725 Scenario Risk/python/simulation/simulation_string.py
@@ -46,12 +46,14 @@ class SimulationString(Simulator):
         data = []
 
         # Run the simulation for at least some time. Stop the simulation if the distance has been
-        # increasing between all vehicles.
+        # increasing between all vehicles or all vehicles impacted.
         distance_increasing = ignore_stop.copy()
         distances = np.ones(len(self.vehicles)-1)*1000
         mindistances = distances.copy()
+        minttcs = distances.copy()
         impact_speeds = np.zeros(len(self.vehicles)-1)
-        while time < self.min_simulation_time or not np.all(distance_increasing):
+        while time < self.min_simulation_time or (not np.all(distance_increasing) and
+                                                  not np.all(impact_speeds)):
             # Update the distances.
             for i, (leader, follower) in enumerate(zip(self.vehicles[:-1], self.vehicles[1:])):
                 distance = leader.state.position - follower.state.position
@@ -62,6 +64,11 @@ class SimulationString(Simulator):
                 if not ignore_stop[i]:
                     distance_increasing[i] = distances[i] <= distance + 0.001  # Add some margin
                 distances[i] = distance
+                if np.abs(leader.state.speed - follower.state.speed) > 1e-5:
+                    ttc = ((follower.state.position - leader.state.position) /
+                           (leader.state.speed - follower.state.speed))
+                    if 0 < ttc < minttcs[i]:
+                        minttcs[i] = ttc
 
             time += self.vehicles[1].parms.timestep
             self.vehicles[0].step_simulation(time)
@@ -101,7 +108,7 @@ class SimulationString(Simulator):
             ax4.legend()
             plt.tight_layout()
 
-        return np.array([mindistances[i] if mindistances[i] > 0 else -impact_speeds[i]
+        return np.array([minttcs[i] if mindistances[i] > 0 else -impact_speeds[i]
                          for i in range(len(self.vehicles)-1)])
 
     def init_simulation(self, **kwargs) -> None:
