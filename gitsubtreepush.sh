2978bcb8b2cab516798e1c8721ff9dc8e9526080
Erwin de Gelder <erwin.degelder@tno.nl>
2021-03-18 10:25:26 +0100

    Update git subtrees.

--- gitsubtreepush.sh
+++ gitsubtreepush.sh
@@ -1,8 +1,9 @@
 git subtree push --prefix="20191113 Journal Scenario Risk" scenariorisk master
 git subtree push --prefix="20191010 Scenario Mining" scenariomining master
-git subtree push --prefix="20180319 Completeness/stats" stats master
-# git subtree push --prefix="20190725 Scenario Risk/python/stats" stats master
-# git subtree push --prefix="20200729 Domain Model" domainmodel master
-# git subtree push --prefix="20201126 Risk Metric" riskmetric master
+# git subtree push --prefix="20180319 Completeness/stats" stats master
+git subtree push --prefix="20190725 Scenario Risk/python/stats" stats master
+git subtree push --prefix="20200729 Domain Model" domainmodel master
+git subtree push --prefix="20201126 Risk Metric" riskmetric master
 # git subtree push --prefix="20201126 Anomaly Detection" anomalydetection master
-# git subtree push --prefix="20201207 Risk Quantification Paper" riskquantification master
+git subtree push --prefix="20201207 Risk Quantification Paper" riskquantification master
+git subtree push --prefix="20190725 Scenario Risk/python" risk master
